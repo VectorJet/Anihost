2aa6240d6fb75928debe5929e43615f666b00207
tammy-kun <tanishqtagde2505@gmail.com>
2026-01-22 18:02:19 +0530

    pre-initial-release

--- api/dist/api/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const GET: (req: Request) => Response | Promise<Response>;
-export declare const POST: (req: Request) => Response | Promise<Response>;
-export declare const PATCH: (req: Request) => Response | Promise<Response>;
-export declare const PUT: (req: Request) => Response | Promise<Response>;
-export declare const OPTIONS: (req: Request) => Response | Promise<Response>;
